5b0a85f170275ded4272eed3caf1ff6e624582a6
Anton Katlyar <katlyar.anton@mail.ru>
2020-10-02 02:01:41 +0300

    Add files via upload

---
+++ precache-manifest.c33f2da44fdb504f479e2add3e37cc0a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a9e2e415390878ec228615f7dd830cd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "890f3e416546c1d4fd82",
+    "url": "/static/css/2.97ca6098.chunk.css"
+  },
+  {
+    "revision": "58ac29a483cb4412aafd",
+    "url": "/static/css/main.14d21cfa.chunk.css"
+  },
+  {
+    "revision": "890f3e416546c1d4fd82",
+    "url": "/static/js/2.141ce1e1.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.141ce1e1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "58ac29a483cb4412aafd",
+    "url": "/static/js/main.54432c14.chunk.js"
+  },
+  {
+    "revision": "fe3d2316098b4b08bc2f",
+    "url": "/static/js/runtime-main.5bcb08bf.js"
+  }
+]);
